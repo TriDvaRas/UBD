197d9d5a2ad7d96fdaa440f744abb92d0dafa38f
TriDvaRas <47600110+TriDvaRas@users.noreply.github.com>
2021-03-04 13:28:54 +0200

    +up

---
+++ INSERT POST.sql
@@ -0,0 +1,36 @@
+--INSERT posts(title,salary)
+--VALUES (
+--	'System Administrator',
+--	9543.42
+--);
+
+INSERT employees(
+	fullname,
+	birthdate,
+	email,
+	employment_date,
+	team_id,
+	post_id
+)
+VALUES (
+	'Ддося Хацапетова',
+	'1909-09-12',
+	'popizi2@mail.mail',
+	'2009-06-03',
+	2,
+	2
+);
+
+
+--INSERT teams(
+--	title
+--)
+--VALUES (
+--	'Flexmatics'
+--);
+--SELECT * FROM teams; 
+
+--ALTER TABLE posts
+--ADD UNIQUE(title);
+
+--SELECT * FROM posts;
---
+++ INSERT departments.sql
@@ -0,0 +1,21 @@
+USE ubd;
+
+--INSERT departments(
+--title,
+--office_adress,
+--manager_id
+--) VALUES (
+--	'Отдел 98',
+--	'ул. Деревни 88',
+--	8
+--);
+--INSERT projects(
+--	title,
+--	deadline
+--) VALUES(
+--	'Dead End',
+--	'2021-03-19'
+--);
+--UPDATE teams SET department_id=2 Where id=2;
+SELECT * FROM teams;
+
